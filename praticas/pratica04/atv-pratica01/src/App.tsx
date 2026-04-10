cbfa9efa632b87b512b7f29fd2919483ad131d56
isabellyleal <isabelly.lmoura@gmail.com>
2026-04-09 21:20:34 -0300

    finalizado ate a pratica 27

--- praticas/pratica04/atv-pratica01/src/App.tsx
+++ praticas/pratica04/atv-pratica01/src/App.tsx
@@ -1,4 +1,6 @@
 // App.tsx
+import { createRoot } from 'react-dom/client';
+import { StrictMode } from 'react';
 import { Container } from './components/Container';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
@@ -11,12 +13,19 @@ import { Footer } from './components/Footer';
 
 import './styles/theme.css';
 import './styles/global.css';
-
+ createRoot(document.getElementById('root')!).render
 
 export function App() {
+
   return (
     <>
-      {/* Seção 1: Logo (Usando div no lugar de Container) */}
+     
+  <>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </>
+
       <Container>
         <Logo />
       </Container>
