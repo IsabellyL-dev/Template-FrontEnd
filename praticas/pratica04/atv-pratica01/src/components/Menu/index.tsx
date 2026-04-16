1b4df9af15ac7392b872ad106f0423bdde3957fb
IsabellyL-dev <isabelly.lmoura@gmail.com>
2026-04-16 13:12:39 -0300

    finalizado até a prática 30

--- praticas/pratica04/atv-pratica01/src/components/Menu/index.tsx
+++ praticas/pratica04/atv-pratica01/src/components/Menu/index.tsx
@@ -11,6 +11,7 @@ import { useState, useEffect } from 'react';
 type AvailableThemes = 'dark' | 'light';
 
 export function Menu() {
+<<<<<<< HEAD
   // 1. Busca o valor inicial do localStorage
   const [theme, setTheme] = useState<AvailableThemes>(() => {
     const storageTheme = localStorage.getItem('theme');
@@ -18,19 +19,44 @@ export function Menu() {
   });
 
   // 2. Mapeamento de ícones (Evita ifs/ternários no JSX)
+=======
+  // 1. Inicialização preguiçosa buscando do localStorage
+  const [theme, setTheme] = useState<AvailableThemes>(() => {
+    const storageTheme =
+      (localStorage.getItem('theme') as AvailableThemes) || 'dark';
+    return storageTheme;
+  });
+
+  // 2. Dicionário de ícones baseado no tema atual
+>>>>>>> b7098f4 (finalizado até a prática 30)
   const nextThemeIcon = {
     dark: <SunIcon />,
     light: <MoonIcon />,
   };
 
+<<<<<<< HEAD
+=======
+  // 3. Função pura de atualização de estado
+>>>>>>> b7098f4 (finalizado até a prática 30)
   function handleThemeChange(
     event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
   ) {
     event.preventDefault();
+<<<<<<< HEAD
     setTheme(prevTheme => (prevTheme === 'dark' ? 'light' : 'dark'));
   }
 
   // 3. Efeito colateral: Muda o HTML e salva no localStorage
+=======
+
+    setTheme(prevTheme => {
+      const nextTheme = prevTheme === 'dark' ? 'light' : 'dark';
+      return nextTheme;
+    });
+  }
+
+  // 4. Efeito Colateral: Aplica no HTML e salva no Storage
+>>>>>>> b7098f4 (finalizado até a prática 30)
   useEffect(() => {
     document.documentElement.setAttribute('data-theme', theme);
     localStorage.setItem('theme', theme);
@@ -72,7 +98,10 @@ export function Menu() {
         title='Mudar Tema'
         onClick={handleThemeChange}
       >
+<<<<<<< HEAD
         {/* Renderiza o ícone dinamicamente baseado na chave atual */}
+=======
+>>>>>>> b7098f4 (finalizado até a prática 30)
         {nextThemeIcon[theme]}
       </a>
     </nav>
